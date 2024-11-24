662b56b67828b186e67ca145e79f40aa1f62cf97
Tran Ngoc Hung <kiaisoft.hung.tran@gmail.com>
2024-11-24 15:31:37 +0700

    refactor/packages

---
+++ LAB03/OtherProjects/hust/soict/dsai/lab01/BaseClass.java
@@ -0,0 +1,9 @@
+package hust.soict.dsai.lab01;
+class BaseClass {
+private float x = 1.0f;
+float getVar() { return x; }
+}
+class SubClass extends BaseClass {
+private float x = 2.0f;
+// insert code here
+}
